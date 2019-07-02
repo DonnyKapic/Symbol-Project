5f6e744d37fdf42c14df7057edb6eb5dc607bc00
DonnyKapic <52429454+DonnyKapic@users.noreply.github.com>
2019-07-02 13:07:14 -0500

    Add files via upload

--- bracket.cpp
+++ bracket.cpp
@@ -11,39 +11,37 @@ int main(){
 	cout << "Please enter a test string.\n";
 	cin >> test;
 
-	//Will add user input feature in future update
 	cout << "The test string is: " << test << endl;
 
-	//If odd, invalid
+	//if number of symbols is odd do not accept
 	if(test.size() % 2 != 0){
 		cout << "Invalid: Odd number of symbols.\n";
 		return 0;
 	}
 
-	int open = 0;
-	int closed = 0;
+	int equality = 0;
 
 	//iterate through string
 	for(auto it = test.begin(); it != test.end(); it++)
 		if(*it == '(' || *it == '[' || *it == '{'){	//open symbols
-			open++;
+			equality++;
 			mystack.push(*it);
 			printStack(mystack);
 		}
 		else if(*it == ')' || *it == ']' || *it == '}'){	//closed symbols
 			switch(*it){					//check for matching open symbol
 				case ')': if(mystack.top() == '('){
-						closed++;
+						equality--;
 						mystack.pop();
 					  };
 					  break;
 				case ']': if(mystack.top() == '['){
-					  	closed++;
+					  	equality--;
 						mystack.pop();
 					  };
 					  break;
 				case '}': if(mystack.top() == '{'){
-						closed++;
+						equality--;
 						mystack.pop();
 					  };
 					  break;
@@ -55,12 +53,7 @@ int main(){
 			return 0;
 		}
 		
-
-
-	cout << "\nOpen: " << open << endl;
-	cout << "Closed: " << closed << endl;
-
-	if(open == closed)
+	if(equality == 0)
 		cout << "SUCCESS. This input is legal.\n";
 	else
 		cout << "This input is ILLEGAL.\n";
